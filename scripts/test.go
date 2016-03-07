b3a45925f863b701a2139aa94942b0e5390a674b
Ulysses Aalto <uaalto@getlantern.org>
2016-03-07 04:47:27 -0500

    Benchmarking

---
+++ scripts/test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/satori/go.uuid"
+)
+
+var addr = flag.String("addr", "localhost:10000", "http service address")
+
+func createWs(interrupt *chan os.Signal) {
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/subscribe"}
+	//log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer c.Close()
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(uuid.NewV4().String()))
+			//err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-*interrupt:
+			log.Println("interrupt")
+			// To cleanly close a connection, a client should send a close
+			// frame and wait for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			c.Close()
+			return
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	for i := 0; i < 40000; i++ {
+		go createWs(&interrupt)
+		time.Sleep(time.Millisecond * 10)
+	}
+
+	select {}
+}
